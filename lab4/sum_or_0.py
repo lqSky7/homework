a0c54afee1dd121a3953177552dc701b4a49bd10
lqsky7 <catinice@outlook.com>
2024-08-19 22:40:46 +0530

    feat: Add Python scripts for sum, even numbers, odd numbers, and table
    - Added `sum_or_0.py` script to calculate the sum and average of a series of numbers.
    - Added `first10even.py` script to print the first 10 even numbers.
    - Added `first10odd.py` script to print the first 10 odd numbers.
    - Added `table.py` script to print the multiplication table of a given number.

--- lab4/sum_or_0.py
+++ lab4/sum_or_0.py
@@ -1,12 +1,13 @@
 sum = 0
 average = 0
-
+iterator = 0
 while True:
     k = int(input())
     if k == 0:
-        average = sum / 2       
+        average = sum / iterator      
         print(sum, average)
         break;
     else:
+        iterator += 1
         sum += k
         continue

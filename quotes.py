47c6472a2ead65b453e206de15cf635a91e80871
lqsky7 <catinice@outlook.com>
2024-08-19 23:06:36 +0530

    Add quotes.py file with quote printing functionality

---
+++ quotes.py
@@ -0,0 +1,5 @@
+quote = "hi"
+print("Shakespeare once said, '" + quote + "'") # use this '" two times
+
+#double quotes
+print("Shakespeare once said, \"" + quote + "\"")  # use this \" two times and " for quoted text"

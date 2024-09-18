cae5b09a38c5bc79324892d2f475e0e58023a420
Diljot Singh <catinice@outlook.com>
2024-09-18 12:55:38 +0530

    Update revNumExceptLast2.py: pseudocode

--- PYTHON_BEFORE_SEM2/FUNCTIONS/revNumExceptLast2.py
+++ PYTHON_BEFORE_SEM2/FUNCTIONS/revNumExceptLast2.py
@@ -13,3 +13,25 @@ rev += n[-1]
     
 rev += n[-2]
 print(rev)
+
+#pseudocode:
+
+```
+input a string and store it in 'n'
+If the length of 'n' is less than or equal to 2:
+    Print "invalid"
+    Exit the program
+
+Initialize an empty string 'rev'
+
+If the length of 'n' is exactly 3:
+    Print 'n'
+
+For each 'i' in the range from 0 to the length of 'n' minus 2:
+    Append the (length of 'n' - 3 - i)-th character of 'n' to 'rev'
+
+Append the last character of 'n' to 'rev'
+Append the second last character of 'n' to 'rev'
+
+Print 'rev'
+```

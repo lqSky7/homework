7688382754e85ffb2144124763b4229e80136f4f
Diljot Singh <catinice@outlook.com>
2024-09-18 12:52:45 +0530

    Update ListOperations.py: pseudocode

--- PYTHON_BEFORE_SEM2/FUNCTIONS/ListOperations.py
+++ PYTHON_BEFORE_SEM2/FUNCTIONS/ListOperations.py
@@ -19,3 +19,27 @@ while(ll == 1):
     ll = int(input("press 1 to run the loop again, 0 to exit"))
     if(ll==0):
         break
+
+
+#PSEUDOCODE 
+# Initialize list 'li' with ["cookie", "cake", "chocolate"]
+# Set 'll' to 1
+
+# While 'll' equals 1:
+#     Ask user for input (1 to view list, 2 to append, 3 to delete)
+    
+#     If input is 1:
+#         Display the list 'li'
+    
+#     If input is 2:
+#         Ask user for an element to add
+#         Append the element to the list 'li'
+    
+#     If input is 3:
+#         Ask user for an element to delete
+#         Remove the element from the list 'li'
+    
+#     Ask user if they want to continue (1 to repeat, 0 to exit)
+    
+#     If user input is 0:
+#         Exit the loop

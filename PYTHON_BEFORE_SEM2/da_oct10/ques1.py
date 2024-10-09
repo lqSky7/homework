a55cc81cc3c2870b2579cd474383cb76ff6b5676
Diljot Singh <catinice@outlook.com>
2024-10-09 20:45:50 +0530

    Fix q1 logic, this makes more sense

--- PYTHON_BEFORE_SEM2/da_oct10/ques1.py
+++ PYTHON_BEFORE_SEM2/da_oct10/ques1.py
@@ -1,3 +1,12 @@
-d = {"physics": 5, "mathematics": 3, "chemistry": 4, "computer science": 2}
-f = sum(d.values())
-print(f)
+physics = [1, 0, 1, 0, 1]
+math = [0, 1, 1, 0, 0]
+chemistry = [0, 0, 1, 0, 1]
+computer_science = [1, 0, 0, 1, 1]
+
+total_failures = 0
+for i in range(len(physics)):
+    if physics[i] == 1 or math[i] == 1 or chemistry[i] == 1 or computer_science[i] == 1:
+        total_failures += 1
+
+print(" number  of students who failed total_failures)
+

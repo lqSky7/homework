cc0988275bbb3b1943ae569178d17778542c5698
lqsky7 <catinice@outlook.com>
2024-10-22 21:09:15 +0530

    wated much more time on this than i initially expected....

--- PYTHON_BEFORE_SEM2/DA_21stOct/test.py
+++ PYTHON_BEFORE_SEM2/DA_21stOct/test.py
@@ -1,8 +1,8 @@
 number_ofInputs = 1
 for i in range(number_ofInputs):
     length =  5
-    wordtoChange =  "kiopl"
-    wordtoMatch = "pkilo"
+    wordtoChange =  "lhoel"
+    wordtoMatch = "hello"
 
     # reject if impossible:
     #this logic is bad since different combinations may have same ascii, but too lazy to change it. 
@@ -22,7 +22,7 @@ for i in range(number_ofInputs):
         print(-1)
         exit()
 
-
+movecnt = 0
 
 # main logic
 wclist = list(wordtoChange)
@@ -31,11 +31,20 @@ if(wordtoMatch[0] != wordtoChange[0]):
     trgetIndex = wclist.index(wordtoMatch[0])
     wclist.insert(0, wordtoMatch[0])
     wclist.pop(trgetIndex+1)
-    
+    movecnt = movecnt + 1
+
 # we match position by position, and throw any unmatched letter to the end, it'll all come together automatically. only if our first letter is unmatched we move stuff to front, otherwise we always pop and move stuff to the end.
-for i in range(length-1):   
-    if(wordtoMatch[i] != wordtoChange[i]):
-        pass
-    if(wordtoChange[i] == wordtoMatch[i]):
-        pass
+for i in range(0, length):
+    
+    if(''.join(wclist) == wordtoMatch):
+        break
+
+    if(wordtoMatch[i] != wclist[i]):
+        idx = wclist.index(wordtoMatch[i])
+        wclist.append(wclist[idx])
+        wclist.pop(idx)
+        movecnt += 1
+        
+print(wordtoMatch)
+print(movecnt)
 print(''.join(wclist))

755819f9325964feaab176eb025f80d414260947
lqsky7 <catinice@outlook.com>
2025-03-30 00:07:49 +0530

    add DA2 implementation with Month and Week classes, and VSCode configuration files

--- sem2/DA2/task.cpp
+++ sem2/DA2/task.cpp
@@ -1,8 +1,10 @@
 #include "task.h"
+#include <string>
 #include <vector>
+
 int main()
 {
-
+    vector<string> week = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
     vector<string> months = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "nov", "Dec"};
     // use cpp v11 else vector cant have aggrgate declaration itll throw error
     Month **MonthObjects = new Month *[12];
@@ -10,5 +12,12 @@ int main()
     {
         string currM = months[i]; // it doesnt waste space, will be auto optimised by compiler. wrote this for better clarity
         MonthObjects[i] = new Month(currM);
+    }
+
+    Week **WeekObjects = new Week *[7];
+    for (int i = 0; i < 7; i++)
+    {
+        string currW = week[i];
+        WeekObjects[i] = new Week(currW);
     }
 }
--- sem2/DA2/task.h
+++ sem2/DA2/task.h
@@ -1,36 +1,42 @@
 #include <string>
 using namespace std;
 
-class Task{
+class Task
+{
 private:
-    static inline int _id = 0;
+    static int _id;
 
 public:
     int priority; // from 0 to 2
     string description;
-    Task(){} // so if nothing give to task it doesnt throw error , we can add tasks later using friend functions
-    Task(int priority, string description) : priority(priority), description(description){
+    inline void initStatic()
+    {
+        _id = 0;
+    };
+    Task() {} // so if nothing give to task it doesnt throw error , we can add tasks later using friend functions
+    Task(int priority, string description) : priority(priority), description(description)
+    {
         _id++;
     }
 };
 
-class Month: public Task //monthly tasks
+class Month : public Task // monthly tasks
 {
-    public:
+public:
     string month;
     friend void editDescription(string newDes);
     friend void editPriority(int priority);
-    Month(string Month): month(Month){}
-    Month(string description, int priority, string month): Task(priority, description), month(month){}
-
+    Month(string Month) : month(Month) {}
+    Month(string description, int priority, string month) : Task(priority, description), month(month) {}
 };
 
-class Week: public Task // weekly tasks
+class Week : public Task // weekly tasks
 {
-    public:
+public:
     string week;
     friend void editDescription(string newDes);
     friend void editPriority(int priority);
-    Week(string week): week(week){}
-    Week(string description, int priority, string week): Task(priority, description), week(week){}
+    Week(string week) : week(week) {}
+    Week(string description, int priority, string week) : Task(priority, description), week(week) {}
 };
+

e87f30e4c7cb31df0cc039bc44a9315f2d929756
lqsky7 <catinice@outlook.com>
2025-03-30 00:19:56 +0530

    add edit functions for Month and Week classes

--- sem2/DA2/task.cpp
+++ sem2/DA2/task.cpp
@@ -2,6 +2,25 @@
 #include <string>
 #include <vector>
 
+
+void editDescription(Month &mon, string newDes)
+{
+    mon.description = newDes;
+}
+void editPriority(Month &mon, int priority)
+{
+    mon.priority = priority;
+}
+
+void editDescription(Week &mon, string newDes)
+{
+    mon.description = newDes;
+}
+void editPriority(Week &mon, int priority)
+{
+    mon.priority = priority;
+}
+
 int main()
 {
     vector<string> week = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};

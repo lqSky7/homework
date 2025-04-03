7bc8a445c838188a96cc202ea2bbec882b87d427
lqsky7 <catinice@outlook.com>
2025-04-03 10:50:39 +0530

    setup script for Python 3.9 installation

---
+++ docker/setup.sh
@@ -0,0 +1,7 @@
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install python3.9
+sudo apt install python3.9-venv
+sudo apt install python3.9-pip
+python3.9 -m ensurepip --upgrade

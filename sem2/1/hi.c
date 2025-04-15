ce69deb72f662196cb8e82119ef1b96e5566d5d5
lqsky7 <catinice@outlook.com>
2025-04-15 21:50:41 +0530

    codebase cleanup
    Repository: homework
    Branch: main
    Commit count in branch: 73
    Total repo commits: 73
    Changes:  47 files changed, 1 insertion(+), 777 deletions(-)
    File types modified:
      - 26 files with extension .c
      - 1 files with extension .cpp
      - 2 files with extension .DS_Store
      - 1 files with extension .h
      - 3 files with extension .json
      - 1 files with extension .png
    System: Darwin 24.5.0 arm64
    Memory available: 42274. pages
    Timestamp: 2025-04-15 21:50:42 IST
    Shell: /bin/zsh
    Change-Id: I237964c704be936dff27f51469681a92

--- sem2/1/hi.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char a[50];
-    printf("Enter your name:\n");
-    scanf("%s", a);
-    printf("Hello %s! Welcome to BCSE102L course.", a);
-    return 0;
-}

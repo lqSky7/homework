ac90aa2c33455b763c93eb45819ac9ecae0f34d2
lqsky7 <catinice@outlook.com>
2025-04-15 21:49:25 +0530

    update requirements and remove deprecated setup script
    Repository: homework
    Branch: main
    Commit count in branch: 72
    Total repo commits: 72
    Changes:  4 files changed, 69 insertions(+), 13 deletions(-)
    File types modified:
      - 1 files with extension .DS_Store
      - 1 files with extension .sh
      - 1 files with extension .txt
    System: Darwin 24.5.0 arm64
    Memory available: 5369. pages
    Timestamp: 2025-04-15 21:49:25 IST
    Shell: /bin/zsh
    Change-Id: I1af4508a23d34fc3def5ad04e238268a

--- docker/setup.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt update
-sudo apt install python3.9
-sudo apt install python3.9-venv
-sudo apt install python3.9-pip
-python3.9 -m ensurepip --upgrade
